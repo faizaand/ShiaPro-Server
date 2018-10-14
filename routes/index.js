a4aa9011af93926722c6ac640541995246a1ff22
Faizaan <fdatoo7@gmail.com>
2018-10-13 20:54:01 -0400

    Add a comment

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 var fs = require('fs');
 
+// Instead of manually going in and adding all of the new route files
+// to my app.js, I opted to have it dynamically require all of the route
+// files in our 'routes/' directory.
+
 module.exports = function(app) {
     fs.readdirSync(__dirname).forEach(function(file) {
         if(file == "index.js") return;
